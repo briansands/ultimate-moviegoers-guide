10462c9f1953014c614a763199c8ecc4405c97f1
Brian Sands <bsands@costar.com>
2019-04-06 22:18:50 -0400

    setup routes, generate base components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { NowPlayingComponent } from './pages/now-playing/now-playing.component';
+import { PopularComponent } from './pages/popular/popular.component';
+import { TopRatedComponent } from './pages/top-rated/top-rated.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: 'now-playing', component: NowPlayingComponent },
+  { path: 'popular', component: PopularComponent },
+  { path: 'top-rated', component: TopRatedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { NowPlayingComponent } from './pages/now-playing/now-playing.component';
+import { PopularComponent } from './pages/popular/popular.component';
+import { TopRatedComponent } from './pages/top-rated/top-rated.component';
+import { SearchComponent } from './search/search.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DetailsComponent,
+    NowPlayingComponent,
+    PopularComponent,
+    TopRatedComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
