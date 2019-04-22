9d702cfe2b706eca5af88e0e9b8ad8b75ea32c77
Brian Sands <bsandswebdev@gmail.com>
2019-04-22 14:20:08 -0400

    make search bar full width, correct squished images on initial load

--- src/app/models/movies.const.ts
+++ src/app/models/movies.const.ts
@@ -4,4 +4,4 @@ export const NOW_PLAYING = `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&lan
 export const POPULAR = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 export const TOP_RATED = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
 
-export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
