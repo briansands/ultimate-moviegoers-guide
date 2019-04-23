b19aa4438fbddc593e38b815d769e4cc00a07a71
Brian Sands <bsandswebdev@gmail.com>
2019-04-22 23:30:33 -0400

    add cool looking bg image to movie details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { MovieDetails } from 'src/app/models/movie-details.interface';
 import { resolvedMovieDetails } from 'src/app/movies/resolved-movies.operator';
+import { BASE_BG_IMG_URL } from 'src/app/models/movies.const';
 
 @Component({
     selector: 'app-details',
@@ -13,11 +14,17 @@ import { resolvedMovieDetails } from 'src/app/movies/resolved-movies.operator';
 })
 
 export class DetailsComponent {
+    public readonly baseBgImgUrl = BASE_BG_IMG_URL;
+    public bgImgUrl : string;
 
     constructor(private route: ActivatedRoute) { }
 
     public movieDetails$: Observable<MovieDetails> = this.route.data.pipe(
         resolvedMovieDetails(),
     );
+
+    public ngOnInit(): void {
+        this.movieDetails$.subscribe(details => this.bgImgUrl = `url(${this.baseBgImgUrl + details.backdrop_path}) no-repeat center`);
+    }
 
 }
