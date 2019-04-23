c81d9138d690c99c2f441677cd4d6666f900a5c8
Brian Sands <bsandswebdev@gmail.com>
2019-04-22 22:39:43 -0400

    add details button

--- src/app/movies/movie-card/movie-card.component.ts
+++ src/app/movies/movie-card/movie-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Movies } from 'src/app/models/movies.interface';
 import { BASE_IMG_URL } from 'src/app/models/movies.const';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'movie-card',
@@ -11,4 +12,14 @@ export class MovieCardComponent {
     @Input() 
     public movie: Movies;
     public readonly baseImgUrl = BASE_IMG_URL;
+
+    constructor(private router: Router) { }
+
+    public routeToMovieDetails(selectedMovieName: string, selectedMovieId: string) {
+        const movieName = selectedMovieName.replace(/\W/g, '');
+
+        if (selectedMovieId) {
+            this.router.navigate([`details/${movieName}`, selectedMovieId]);
+        }
+    }
 }
